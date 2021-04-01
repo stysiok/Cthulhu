a41d102d0d1e6ded0e5d6a794751bb011a429736
stysiok <stysiok@o2.pl>
2021-04-01 20:05:55 +0200

    trying out few kraken api calls

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import requests
+
+response = requests.get("https://api.kraken.com/0/public/Ticker?pair=BTCEUR")
+print(response.json())
+#Define which crypto to buy
+
+#Auth to kraken
+
+#Perform orders
+
+#Close self
+
