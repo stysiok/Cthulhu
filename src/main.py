d18df0402625c7266f275df9fdff0d372659d233
stysiok <stysiok@o2.pl>
2021-04-05 14:16:16 +0200

    get current price of a currency

--- src/main.py
+++ src/main.py
@@ -8,16 +8,27 @@ krakenApi = krakenex.API()
 jsonObj = ''
 with open('settings.json') as json_file:
     jsonObj = json.load(json_file)
-cryptos = json2Cryptos(jsonObj['ToBuy'])
+assets = json2Assets(jsonObj['ToBuy'])
 
-buyCurrency = 'EUR'
-pairs = map(lambda x: f'{x.currency}{buyCurrency}', cryptos)
-tickerPairs = ','.join(pairs)
+tickerPairs = ','.join(map(lambda x: x.key, assets))
 print(tickerPairs)
-result = krakenApi.query_public(f'Ticker?pair={tickerPairs}', )
-print(result)
+result = krakenApi.query_public(f'Ticker?pair={tickerPairs}')['result']
+
+for a in assets:
+    assetData = ''
+    if a.key in result:
+        assetData = result[a.key]
+    elif a.altKey in result:
+        assetData = result[a.altKey]
+    else:
+        raise Exception('key and alternative key are not part of the dictionary')
+    currentPrice = assetData['c'][0]
+    print(f'{a.key} = {currentPrice} in {a.currency}')
+    a.currentPrice = currentPrice
+
+balance = 100
+
 #Auth to kraken
-krakenApi.load_key('kraken.key')
+# krakenApi.load_key('kraken.key')
 
 #Perform orders
-
--- src/models/kraken.py
+++ src/models/kraken.py
@@ -1,13 +1,20 @@
 import json
 
-class Crypto(object):
-    def __init__(self, currency, min):
+class Asset:
+    currentPrice = -1
+    def __init__(self, crypto, currency, min):
         self.currency = str.upper(currency)
+        self.crypto = str.upper(crypto)
+        self.key = self.crypto + self.currency
+        self.altKey = 'X' + self.crypto + 'Z' + self.currency
         self.min = min
+    
+    def getMinPrice(self):
+        return self.currentPrice * self.min
 
-def json2Cryptos(jsonObj): 
+def json2Assets(jsonObj): 
     arr = []
     for v in jsonObj:
-        crypto = Crypto(v['currency'], v['min'])
-        arr.append(crypto)
+        asset = Asset(v['crypto'], v['currency'], v['min'])
+        arr.append(asset)
     return arr
